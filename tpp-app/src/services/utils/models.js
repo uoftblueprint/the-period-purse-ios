eb108bf2fa865636f35fa900a92d49ebb007fa3b
Merrick Liu <merrickliu168@gmail.com>
2025-01-25 12:23:03 -0500

    fixing Symptoms constructor

--- tpp-app/src/services/utils/models.js
+++ tpp-app/src/services/utils/models.js
@@ -2,14 +2,14 @@
  * The symptoms of each day
  */
 export class Symptoms {
-  constructor(flow = null, mood = null, sleep = null, cramps = null, exercise = null, notes = null) {
+  constructor(flow = null, mood = null, sleep = null, cramps = null, exercise = null, notes = null, ovulating = null) {
     this.flow = flow;
     this.mood = mood;
     this.sleep = sleep;
     this.cramps = cramps;
     this.exercise = exercise;
     this.notes = notes;
-    this.ovulating = null; 
+    this.ovulating = ovulating;
   }
 }
 
