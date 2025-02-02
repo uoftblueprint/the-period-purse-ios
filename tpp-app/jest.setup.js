62bff552a8256bf9bc8b1d1fa6720c32f447cfdf
Mark Oh <mark.oh@mail.utoronto.ca>
2025-02-02 15:06:40 -0500

    removed more test files

--- tpp-app/jest.setup.js
+++
@@ -1,6 +0,0 @@
-jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
-jest.mock('@react-native-community/push-notification-ios', () => ({
-  addNotificationRequest: jest.fn(),
-  removePendingNotificationRequests: jest.fn(),
-  requestPermissions: jest.fn()
-})); 
