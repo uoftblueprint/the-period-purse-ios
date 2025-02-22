4a17b36dd3d7e5de9985a11b9293bb856593487b
Sataphon Obra <puye53991@gmail.com>
2025-02-21 21:58:35 -0500

    bugfix: symtomp selection onboarding

--- tpp-app/src/services/OnboardingService.js
+++ tpp-app/src/services/OnboardingService.js
@@ -108,6 +108,15 @@ export const POSTSymptomsToTrack = async (
   ovulation
 ) =>
   new Promise(async (resolve, reject) => {
+    console.log("Tracking values:", {
+      flow,
+      mood,
+      sleep,
+      cramps,
+      exercise,
+      ovulation,
+    });
+    
     try {
       if (
         [flow, mood, sleep, cramps, exercise, ovulation].some((bool) => bool)
