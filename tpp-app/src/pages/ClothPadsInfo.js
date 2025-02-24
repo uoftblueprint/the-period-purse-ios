e6028b96c2ebe18420e63918ce766acdadd232e0
fredh2006 <fredhe56@gmail.com>
2025-02-23 19:16:26 -0500

    pages without tampons

--- tpp-app/src/pages/ClothPadsInfo.js
+++ tpp-app/src/pages/ClothPadsInfo.js
@@ -1,54 +1,108 @@
+
 import React from "react";
-import { StyleSheet, Text, Image, ImageBackground, SafeAreaView } from "react-native";
+import { StyleSheet, Text, Image, ImageBackground, SafeAreaView, View, ScrollView } from "react-native";
 import ClothImage from "../../assets/InfoPageImages/cloth-pads-2x.png";
+import ClothImage1 from "../../assets/InfoPageImages/cloth1.png";
+import ClothImage2 from "../../assets/InfoPageImages/cloth2.png";
+import ClothImage3 from "../../assets/InfoPageImages/cloth3.png";
+import ClothImage4 from "../../assets/InfoPageImages/cloth4.png";
 import { BackButton } from "../home/components/BackButtonComponent";
 import OnboardingBackground from "../../assets/SplashScreenBackground/colourwatercolour.png";
 import ErrorFallback from "../error/error-boundary";
 import { BackButtonContainer } from "../onboarding/components/ContainerComponents";
-
 export default function ClothPadInfo({ navigation }) {
   return (
     <ErrorFallback>
       <ImageBackground source={OnboardingBackground} style={styles.container}>
         <SafeAreaView pointer-events="box-only" style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <BackButtonContainer>
+          <BackButtonContainer style={styles.backButtonContainer}>
             <BackButton title="" onPress={() => navigation.goBack()} />
           </BackButtonContainer>
-          <Image source={ClothImage} style={styles.image} resizeMode="contain" />
-          <Text style={styles.titleText}>Cloth Pads</Text>
-
-          <Text style={styles.bodyText}>
-            Storytime: here comes your period - reusable cloth pads are a great period product option!
-            {"\n\n"}
-            Cloth pads originated as cloth rags dating back to the 10th century in Ancient Greece. Now they are made of
-            cotton, an absorbent, leak-proof material such as “Zorb,” Polyurethane Laminate (a plastic-like material for
-            the pad’s backing), or amazing bamboo or organic cotton. Most of them have snaps to secure the pad in place,
-            like a pad with wings.
-            {"\n\n"}
-            You need about five cloth pads to wash and use throughout your cycle. They are also a financial investment
-            (around $100 for 5), but cloth pads will last for 2-3 years, depending on personal usage and care.
-          </Text>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <Image source={ClothImage} style={styles.image} resizeMode="contain" />
+            <Text style={styles.titleText}>Cloth Pads</Text>
+            <Text style={styles.bodyText}>
+              Storytime: here comes your period - reusable cloth pads are a great period product option!
+              {"\n\n"}
+              Cloth pads originated as cloth rags dating back to the 10th century in Ancient Greece. Now they are made of
+              cotton, an absorbent, leak-proof material such as “Zorb,” Polyurethane Laminate (a plastic-like material for
+              the pad’s backing), or amazing bamboo or organic cotton. Most of them have snaps to secure the pad in place,
+              like a pad with wings.
+              {"\n\n"}
+              You need about five cloth pads to wash and use throughout your cycle. They are also a financial investment
+              (around $100 for 5), but cloth pads will last for 2-3 years, depending on personal usage and care.
+            </Text>
+            
+            <Text style={styles.titleText}>How to Use</Text>
+            <View style={styles.instructionContainer}>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>1</Text>
+                </View>
+                <Image source={ClothImage1} style={styles.doubleImageFirst} />
+                <Text style={styles.instructionsText}>
+                  Put in underwear and snap into place
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>2</Text>
+                </View>
+                <Image source={ClothImage2} style={styles.instructionImage} />
+                <Text style={styles.instructionsText}>
+                  Wear for 3-5 hours
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>3</Text>
+                </View>
+                <Image source={ClothImage3} style={styles.doubleImage} />
+                <Text style={styles.instructionsText}>
+                  Hand wash OR machine wash
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>4</Text>
+                </View>
+                <Image source={ClothImage4} style={styles.instructionImage} />
+                <Text style={styles.lastInstructionsText}>
+                  Hang to dry
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </ImageBackground>
     </ErrorFallback>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
+  },
+  scrollContainer: {
+    flexGrow: 1,
     justifyContent: "center",
+    alignItems: "center", 
+    paddingTop: "40%",
+    paddingBottom: "30%",
+  },
+  backButtonContainer: {
+    position: "absolute",
+    top: 40, 
+    left: 20,
+    zIndex: 1, 
   },
   image: {
     width: "70%",
     height: "17%",
-    marginBottom: "6%",
   },
   titleText: {
     textAlign: "center",
     fontFamily: "Avenir",
-    marginBottom: "3%",
+    marginBottom: "5%",
     fontWeight: "800",
     fontSize: 34,
     lineHeight: 40,
@@ -61,5 +115,62 @@ const styles = StyleSheet.create({
     letterSpacing: -0.3,
     paddingLeft: "10%",
     paddingRight: "10%",
+    paddingBottom: "10%"
+  },
+  instructionContainer: {
+    width: "90%",
+    marginBottom: 20,
+  },
+  instructionStep: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  numberCircle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#72C6B7",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "5%",
+  },
+  numberText: {
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FCFFFE",
+  },
+  doubleImage: {
+    width: 300,
+    height: 200,
+    marginBottom: "5%",
+  },
+  doubleImageFirst: {
+    width: 350,
+    height: 150,
+    marginBottom: "5%",
+  },
+  instructionImage: {
+    width: 350,
+    height: 250,
+    marginBottom: "5%",
+  },
+  instructionsText: {
+    fontFamily: "Avenir",
+    fontSize: 20,
+    lineHeight: 20,
+    letterSpacing: -0.3,
+    textAlign: "center",
+    paddingTop: "5%",
+    paddingBottom: "5%",
+  },
+  lastInstructionsText: {
+    fontFamily: "Avenir",
+    fontSize: 20,
+    lineHeight: 20,
+    letterSpacing: -0.3,
+    textAlign: "center",
+    paddingBottom: "15%"
   },
 });
--- tpp-app/src/pages/MenstrualDiscInfo.js
+++ tpp-app/src/pages/MenstrualDiscInfo.js
@@ -1,56 +1,134 @@
+
 import React from "react";
-import { StyleSheet, Text, Image, ImageBackground, SafeAreaView, View } from "react-native";
+import { StyleSheet, Text, Image, ImageBackground, SafeAreaView, View, ScrollView } from "react-native";
 import { BackButton } from "../home/components/BackButtonComponent";
 import DiscImage from "../../assets/InfoPageImages/menstrual-disk.svg";
+import DiscImage1 from "../../assets/InfoPageImages/disc1.png";
+import DiscImage2 from "../../assets/InfoPageImages/disc2.png";
+import DiscImage3 from "../../assets/InfoPageImages/disc3.png";
+import DiscImage4 from "../../assets/InfoPageImages/disc4.png";
+import DiscImage5 from "../../assets/InfoPageImages/disc5.png";
+import DiscImage6 from "../../assets/InfoPageImages/disc6.png";
 import OnboardingBackground from "../../assets/SplashScreenBackground/colourwatercolour.png";
 import { BackButtonContainer } from "../onboarding/components/ContainerComponents";
 import ErrorFallback from "../error/error-boundary";
-
 export default function MenstrualDiscInfo({ navigation }) {
   return (
     <ErrorFallback>
       <ImageBackground source={OnboardingBackground} style={styles.container}>
         <SafeAreaView pointer-events="box-only" style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <BackButtonContainer>
+          <BackButtonContainer style={styles.backButtonContainer}>
             <BackButton title="" onPress={() => navigation.goBack()} />
           </BackButtonContainer>
-          <View style={styles.image}>
-            <DiscImage />
-          </View>
-          <Text style={styles.titleText}>Menstrual Disc</Text>
-
-          <Text style={styles.bodyText}>
-            Vibe check! (Because your period’s almost here). Menstrual discs are gaining popularity very quickly and
-            were created less than 30 years ago!
-            {"\n\n"}
-            Unlike menstrual cups, discs don't use suction to stay in place. If you can feel it, try making sure that it
-            is pushed all the way back before tucking it up behind your pubic bone.
-            {"\n\n"}
-            Many menstrual discs are single use, but there are more companies coming on the market introducing reusable
-            menstrual discs. They are made of non-porous medical grade silicone, and like a menstrual cup, should be
-            changed up to every 12 hours. They only come in one size and one shape, so it fits everyone!
-          </Text>
+          <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.image}>
+              <DiscImage />
+            </View>
+            <View style = {styles.infoContainer}>
+              <Text style={styles.titleText}>Menstrual Disc</Text>
+              <Text style={styles.bodyText}>
+                Vibe check! (Because your period’s almost here). Menstrual discs are gaining popularity very quickly and
+                were created less than 30 years ago!
+                {"\n\n"}
+                Unlike menstrual cups, discs don't use suction to stay in place. If you can feel it, try making sure that it
+                is pushed all the way back before tucking it up behind your pubic bone.
+                {"\n\n"}
+                Many menstrual discs are single use, but there are more companies coming on the market introducing reusable
+                menstrual discs. They are made of non-porous medical grade silicone, and like a menstrual cup, should be
+                changed up to every 12 hours. They only come in one size and one shape, so it fits everyone!
+              </Text>
+            </View>
+            <Text style={styles.titleText}>How to Use</Text>
+            <View style={styles.instructionContainer}>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>1</Text>
+                </View>
+                <Image source={DiscImage1} style={styles.doubleImage} />
+                <Text style={styles.instructionsText}>
+                  Fold in half
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>2</Text>
+                </View>
+                <Image source={DiscImage2} style={styles.doubleImage} />
+                <Text style={styles.instructionsText}>
+                  Insert with your fingers
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>3</Text>
+                </View>
+                <Image source={DiscImage3} style={styles.instructionImage} />
+                <Text style={styles.instructionsText}>
+                  Wear up to 12 hours
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>4</Text>
+                </View>
+                <Image source={DiscImage4} style={styles.instructionImage} />
+                <Text style={styles.instructionsText}>
+                  Hook rim with fingers to remove
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>5</Text>
+                </View>
+                <Image source={DiscImage5} style={styles.instructionImage} />
+                <Text style={styles.instructionsText}>
+                  Empty disc
+                </Text>
+              </View>
+              <View style={styles.instructionStep}>
+                <View style={styles.numberCircle}>
+                  <Text style={styles.numberText}>6</Text>
+                </View>
+                <Image source={DiscImage6} style={styles.instructionImage} />
+                <Text style={styles.instructionsText}>
+                  Wash disc
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </ImageBackground>
     </ErrorFallback>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
+  },
+  scrollContainer: {
+    flexGrow: 1,
     justifyContent: "center",
+    alignItems: "center",
+    paddingTop: "60%",
+    paddingBottom: "40%",
+  },
+  backButtonContainer: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+    zIndex: 1,
   },
   image: {
     width: "29%",
     height: "15%",
-    marginBottom: "6%",
+  },
+  infoContainer: {
+    marginTop: "-60%"
   },
   titleText: {
     textAlign: "center",
     fontFamily: "Avenir",
-    marginBottom: "3%",
+    marginBottom: "5%",
     fontWeight: "800",
     fontSize: 34,
     lineHeight: 40,
@@ -63,5 +141,48 @@ const styles = StyleSheet.create({
     letterSpacing: -0.3,
     paddingLeft: "10%",
     paddingRight: "10%",
+    paddingBottom: "10%"
+  },
+  instructionContainer: {
+    width: "90%",
+    marginBottom: 20,
+  },
+  instructionStep: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  numberCircle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#72C6B7",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "5%",
+  },
+  numberText: {
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FCFFFE",
+  },
+  doubleImage: {
+    width: 300,
+    height: 250,
+    marginBottom: "5%",
+  },
+  instructionImage: {
+    width: 300,
+    height: 220,
+  },
+  instructionsText: {
+    fontFamily: "Avenir",
+    fontSize: 20,
+    lineHeight: 20,
+    letterSpacing: -0.3,
+    textAlign: "center",
+    paddingTop: "7%",
+    paddingBottom: "5%",
   },
 });
